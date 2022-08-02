95cfc1a1da0ac3d3eda553b0c5ea2244f3cabcf1
rothberry <phil.roth077@gmail.com>
2022-08-01 18:58:39 -0700

    new

--- src/App.js
+++ src/App.js
@@ -3,30 +3,28 @@ import About from "./About"
 import Home from "./Home"
 import Nav from "./Nav"
 import HumanContainer from "./HumanContainer"
-import { Route, Switch } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
-			<Switch>
+			<Routes>
 				{/* <Route exact path="/about" component={About}/> */}
-				<Route path="/about">
-					<About />
-				</Route>
-				<Route
+				<Route path="/about" element={<About />} />
+				{/* <Route
 					path="/humans"
 					render={(routeProps) => (
 						<HumanContainer {...routeProps} humans={[]} />
 					)}
+				/> */}
+				<Route
+					path="/humans"
+					element={<HumanContainer humans={[{ name: "Jenipher" }]} />}
 				/>
-				{/*  <Route path="/humans">
-				 	<HumanContainer humans={[{ name: "Jenipher" }]} />
-				 </Route> */}
-				<Route path="/">
-					<Home />
-				</Route>
-			</Switch>
+
+				<Route path="/" element={<Home />} />
+			</Routes>
 		</div>
 	)
 }
